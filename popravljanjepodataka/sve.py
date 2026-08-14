ad47c9203bcccab994497dbc2b897ad07d864b3c
necapeca1007 <nevenar2008@gmail.co>
2026-08-15 00:37:28 +0200

    sve stavljeno u jedan fajl, popravljeni kodovi za greske, ispisan grafik sa prognozama i stvarnim podacima, petlja koja vraca vrednosti relativne greske vece od 50

--- popravljanjepodataka/sve.py
+++ popravljanjepodataka/sve.py
@@ -4,15 +4,37 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+#ucitavanje i sredjivanje podataka
+
 df=pd.read_csv("/home/polaznik/Desktop/nevena/TemperatureForecasting/real_data/berlin_data.csv")
 print(df.head())
 
 df = df.iloc[600:]
 
 df = df.reset_index(drop=True)
+total_missing_rows = df.isna().any(axis=1).sum()
+print("hello nema ovoliko", total_missing_rows)
 
-#RELATIVNA
+
+
+#srednja kvadratna greska
+
+y_true = df['temperature_2m']
+y_pred = df['temperature_2m_previous_day1']
+
+
+mse = mean_squared_error(y_true, y_pred)
+
+print("srednja kvadratna greska", mse)
+
 """
+
+
+
+#RELATIVNA GRESKA
+
+###relativna greska za svaki podatak (recorded temp vs predvidjena)
+
 def relativna(temperature_2m, temperature_2m_previous_day1):
  
     y_true = df[temperature_2m]
@@ -21,7 +43,7 @@ def relativna(temperature_2m, temperature_2m_previous_day1):
     y_true_masked = y_true.replace(0, np.nan)
 
     relative_errors = np.abs(
-        (y_true_masked - y_pred) / y_true_masked)
+        (y_true_masked - y_pred)/ y_true_masked )
 
     return relative_errors
 
@@ -33,6 +55,8 @@ greska1d= relativna("temperature_2m", "temperature_2m_previous_day1")
 greska3d= relativna("temperature_2m", "temperature_2m_previous_day3")
 greska5d= relativna("temperature_2m", "temperature_2m_previous_day5")
 greska7d= relativna("temperature_2m", "temperature_2m_previous_day7")
+
+#linijski grafik relativnih greski
 
 plt.figure(figsize=(12, 6))
 
@@ -51,8 +75,9 @@ plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
 
-#srednja relativna
 
+
+###Srednja relativna
 
 relativna1d= relativna("temperature_2m","temperature_2m_previous_day1")
 relativna3d= relativna("temperature_2m","temperature_2m_previous_day3")
@@ -61,7 +86,7 @@ relativna7d= relativna("temperature_2m","temperature_2m_previous_day7")
 
 print("dan1 relativna",relativna1d)
 
-greske = [relativna1d, relativna3d, relativna5d, relativna7d]
+greske = [relativna1d.mean(), relativna3d.mean(), relativna5d.mean(), relativna7d.mean()]
 nazivi = ["1 dan", "3 dana", "5 dana", "7 dana"]
 
 plt.figure(figsize=(8, 5))
@@ -74,16 +99,18 @@ plt.title("srednja relativna greska")
 
 plt.grid(axis="y", alpha=0.3)
 plt.show()
-"""
 
+"""
 #KVADRATNA
 
 #vreme vs greska
 
-def kvadratna(temperature_2m, temperature_2m_previous_day1):
+df["date"] = pd.to_datetime(df["date"])
+
+def kvadratna(danas, predvidjanje):
     
-    y_true = df[temperature_2m]
-    y_pred = df[temperature_2m_previous_day1]
+    y_true = df[danas]
+    y_pred = df[predvidjanje]
 
     squared_errors = (y_true - y_pred) ** 2
 
@@ -114,6 +141,8 @@ plt.tight_layout()
 plt.show()
 
 #srednje kvadratne grseske
+
+"""
 def srednjakvadratna(temperature_2m, temperature_2m_previous_day1):
    
     y_true = df[temperature_2m]
@@ -143,3 +172,31 @@ plt.title("srednja kvadratna greska")
 
 plt.grid(axis="y", alpha=0.3)
 plt.show()
+"""
+
+
+temp = df['temperature_2m']
+prognoza1d = df['temperature_2m_previous_day1']
+prognoza7d = df['temperature_2m_previous_day7']
+
+
+def plot(y1,y2,y3):
+    plt.figure(figsize=(12, 6))
+
+    plt.plot(df["date"], y1, label=" dan")
+    plt.plot(df["date"], y2, label="1 dana")
+    plt.plot(df["date"], y3, label="7 dana")
+
+
+    plt.xlabel("vreme")
+    plt.ylabel("temperatura")
+    plt.title("stvarna temperaura i predikcije")
+    plt.legend()
+    plt.grid(True)
+
+
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
+plot(temp,prognoza1d,prognoza7d)
